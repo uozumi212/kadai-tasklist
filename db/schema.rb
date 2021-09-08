25694174362537e41d5ad959cc064e250471721b
kazuma.uozumi <kazu3278mana@gmail.com>
2021-09-08 10:39:17 +0000

    task-list

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_06_113618) do
+ActiveRecord::Schema.define(version: 2021_09_08_072733) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
