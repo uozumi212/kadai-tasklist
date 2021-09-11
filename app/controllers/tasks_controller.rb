1f21e99394620d80c2722954de0331d341bf8adb
kazuma.uozumi <kazu3278mana@gmail.com>
2021-09-11 10:47:29 +0000

    task-list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -38,7 +38,7 @@ class TasksController < ApplicationController
    end
   end
   def destroy
-       
+       @task.destroy
     flash[:success] = 'task は正常に削除されました'
     redirect_to root_url
   end
