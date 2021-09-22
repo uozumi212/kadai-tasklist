b4b60eeca5d6e269e5f52955e4bf65858b76a4cc
kazuma.uozumi <kazu3278mana@gmail.com>
2021-09-22 07:14:07 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   before_action :correct_user, only: [:destroy, :update, :show, :edit]
   
   def index
-        @pagy,@tasks = pagy(current_user.tasks.order(id: :desc))
+        @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
   end
 
   def show
